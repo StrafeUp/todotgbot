5a261d8bac699f5096758fd3dbc5911d83607a11
StrafeUp <bogdan22693@gmail.com>
2020-07-24 23:45:41 +0300

    Initial commit

---
+++ src/main/java/com/strafeup/todotgbot/TodoistBot.java
@@ -0,0 +1,22 @@
+package com.strafeup.todotgbot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TodoistBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
---
+++ src/test/java/com/strafeup/todotgbot/TodoTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strafeup.todotgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
